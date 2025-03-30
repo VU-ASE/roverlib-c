cca8124326b4f011780baccce8f06232e4539a39
Darian <d.janevski@student.vu.nl>
2025-03-30 15:23:37 +0200

    fix: paths in include statements are now correct
    The paths in the includes were missing the "rovercom/" directory. Now the paths are all correct

--- src/rovercom/outputs/wrapper.pb-c.c
+++ src/rovercom/outputs/wrapper.pb-c.c
@@ -6,7 +6,7 @@
 #define PROTOBUF_C__NO_DEPRECATED
 #endif
 
-#include "outputs/wrapper.pb-c.h"
+#include "rovercom/outputs/wrapper.pb-c.h"
 void   protobuf_msgs__sensor_output__init
                      (ProtobufMsgs__SensorOutput         *message)
 {
